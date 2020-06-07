3d8795d3b789a819489312e8bf25a1b3d1f13784
Alex Adams <alexadams2084@gmail.com>
2020-06-07 22:10:51 +0100

    Added eating :o

--- Cell.py
+++ Cell.py
@@ -1,6 +1,8 @@
 import random
+import math
 class Cell:
-    lifespan = 50
+    lifespan = 500
+    radius = 10
 
     def __init__(self, pos):
         self.pos = pos
@@ -9,3 +11,12 @@ class Cell:
         x, y = random.randint(-5, 5), random.randint(-5, 5)
         new_pos = [self.pos[0] + x, self.pos[1] + y]
         self.pos = new_pos
+
+    def getTouching(self, cell2):
+        if cell2.radius > self.radius:
+            largest_radius = cell2.radius
+        else:
+            largest_radius = self.radius
+
+        distance = math.sqrt((self.pos[0]-cell2.pos[0])**2 + (self.pos[1]-cell2.pos[1])**2)
+        return largest_radius >= distance
--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-import pygame
 import random
+import pygame
 from Cell import Cell
 
 pygame.init()
@@ -27,17 +27,33 @@ while RUNNING:
     y = random.randint(0, 600)
     cells.append(Cell([x, y]))
 
+    # move the cells
     for cell in cells:
         cell.move()
 
+
+    # draw the cells
     for cell in cells:
-        pygame.draw.circle(screen, (255, 0, 0), cell.pos, 10)
+        pygame.draw.circle(screen, (255, 0, 0), cell.pos, cell.radius)
 
+    # find any touching cells and cull the weakest >:)
+    for cell in cells:
+        for cell2 in cells:
+            if not cell == cell2:
+                if cell.getTouching(cell2):
+                    pygame.draw.circle(screen, (0, 255, 0), cell.pos, cell.radius)
+                    if cell.radius > cell2.radius:
+                        cells.remove(cell2)
+                        cell.radius += 2
+                    else:
+                        cells.remove(cell)
+                        cell2.radius += 2
 
-
+    # cull the cells :c
     for cell in cells:
         cell.lifespan -= 1
         if cell.lifespan <= 0:
+            print("cell died of old age at size", cell.radius)
             cells.remove(cell)
 
     pygame.display.update()
