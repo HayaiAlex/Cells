bce5f86fe55621f440a6eafe0a5ecd9aa0d4d0f9
Alex Adams <alexadams2084@gmail.com>
2020-06-08 17:03:00 +0100

    Cell's now change colour as they age

--- Cell.py
+++ Cell.py
@@ -2,20 +2,27 @@ import random
 import math
 class Cell:
     lifespan = 500
+    age = 1
     radius = 10
     angle = 0 # in radians
     turn = 0
     speed = 3
     turn_cooldown = 0
 
+    starting_colour = (255, 0, 0)
+    final_colour = (0, 0, 255)
+
     def __init__(self, pos):
         self.pos = pos
         self.angle = random.uniform(0, 2*math.pi)
+        self.colour = self.starting_colour
 
     def pick_new_turn(self):
         self.turn = random.uniform(-0.5, 0.5)
 
     def move(self):
+        # idea: add obsticles (scenery) for cells to move around
+        # cells should try move to/eat smaller cells and flee from larger ones
         if self.turn_cooldown <= 0:
             self.pick_new_turn()
             self.turn_cooldown = 5
@@ -28,6 +35,15 @@ class Cell:
         new_pos = [int(round(new_x)), int(round(new_y))]
         self.pos = new_pos
 
+    def birthday(self):
+        self.age += 1
+
+        # Finds the difference between starting_colour and final_colour
+        # Reduces it to the proportion of a cell's age through its full lifespan (1.4 the change for only 1/4 through its life)
+        # Subtracts this new difference from the starting_colour to get the new_colour 
+        proportion_through_life = self.age / self.lifespan
+        new_colour = tuple(x-((x-y) * proportion_through_life) for x, y in zip(self.starting_colour, self.final_colour))
+        self.colour = new_colour
 
     def getTouching(self, cell2):
         distance_between_points = math.sqrt((self.pos[0]-cell2.pos[0])**2 + (self.pos[1]-cell2.pos[1])**2)
--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ cells = []
 RUNNING = True
 clock = pygame.time.Clock()
 while RUNNING:
-    clock.tick(10)
+    clock.tick(20)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,29 +44,30 @@ while RUNNING:
             cell.pos[0] -= screen.get_width()
         if cell.pos[1] > screen.get_height():
             cell.pos[1] -= screen.get_height()
-        pygame.draw.circle(screen, (255, 0, 0), cell.pos, cell.radius)
+        pygame.draw.circle(screen, cell.colour, cell.pos, cell.radius)
 
     # find any touching cells and cull the weakest >:)
     for cell in cells:
         for cell2 in cells:
             if not cell == cell2:
                 if cell.getTouching(cell2):
-                    pygame.draw.circle(screen, (0, 255, 0), cell.pos, cell.radius)
                     if cell.radius > cell2.radius:
                         # these cell exist checks dont fix the bug here
-                        # sometimes tries to remove a cell that doesn't exist
-                        if cell2:
+                        # sometimes tries to get type of a cell that doesn't exist
+                        if type(cells.index(cell2)) == int:
                             cells.remove(cell2)
                         cell.radius += 2
+                        pygame.draw.circle(screen, (0, 150, 0), cell2.pos, cell2.radius)
                     else:
-                        if cell:
+                        if type(cells.index(cell)) == int:
                             cells.remove(cell)
                         cell2.radius += 2
+                        pygame.draw.circle(screen, (0, 150, 0), cell.pos, cell.radius)
 
     # cull the cells :c
     for cell in cells:
-        cell.lifespan -= 1
-        if cell.lifespan <= 0:
+        cell.birthday()
+        if cell.age > cell.lifespan:
             print("cell died of old age at size", cell.radius, "rip")
             cells.remove(cell)
 
